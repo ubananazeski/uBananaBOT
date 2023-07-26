0af389b8b0b6dadbbe0b5468c93149dd0a2eed4e
uBanana <78665664+ubananazeski@users.noreply.github.com>
2023-07-26 20:02:33 -0300

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -2,15 +2,15 @@ const { glob } = require("glob");
 const { promisify } = require("util");
 const { Client } = require("discord.js");
 const globPromise = promisify(glob);
-
+const fs = require("fs")
 /**
  * @param {Client} client
  */
 module.exports = async (client) => {
-
     const slashCommands = await globPromise(
         `${process.cwd()}/SlashCommands/*/*.js`
     );
+    console.log('Comandos carregados com sucesso.');
 const arrayOfSlashCommands = [];
 slashCommands.map((value) => {
     const file = require(value);
