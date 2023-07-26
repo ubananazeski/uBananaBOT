f50c9aeecaacab8c48dbdc0221a6bd6cd6099097
uBanana <78665664+ubananazeski@users.noreply.github.com>
2023-07-26 20:00:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
-const { Client, Intents, GatewayIntentBits, ActivityType, PermissionFlagsBits } = require('discord.js');
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js")
+const { Client, Intents, GatewayIntentBits, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ActionRow } = require("discord.js")
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require("./config.json");
-
+const fs = require("fs")
 client.login(config.token);
-
-
-
 module.exports = client;
 client.commands = new Discord.Collection();
 client.slashCommands = new Discord.Collection();
@@ -18,44 +16,8 @@ const { promisify } = require("util");
 
 const globPromise = promisify(glob);
 
-client.once('ready', async () => {
-    console.log(`✅ - Logado em ${client.user.username} com sucesso! Estou em ${client.guilds.cache.size} servidores!`)
-    client.user.setPresence({
-        activities: [{ name: `15 comandos para ${client.guilds.cache.size} servidores!`, type: ActivityType.Playing }],
-        status: 'idle',
-      });
-})
-
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.guild) return;
-
-    if (interaction.isCommand()) {
-
-        const cmd = client.slashCommands.get(interaction.commandName);
-
-        if (!cmd)
-            return;
-
-        const args = [];
-
-        for (let option of interaction.options.data) {
-
-            if (option.type === "SUB_COMMAND") {
-                if (option.name) args.push(option.name);
-                option.options?.forEach((x) => {
-                    if (x.value) args.push(x.value);
-                });
-            } else if (option.value) args.push(option.value);
-        }
-
-        cmd.run(client, interaction, args);
-    }
-
-    if (interaction.isContextMenuCommand()) {
-        await interaction.deferReply({ ephemeral: false });
-        const command = client.slashCommands.get(interaction.commandName);
-        if (command) command.run(client, interaction);
-
-    }
-});
+fs.readdir('./events/', (err, file) => {
+  file.forEach(event => {
+    require(`./events/${event}`)
+  })
+}) 
